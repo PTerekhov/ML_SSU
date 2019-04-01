9f10c29bb25cd6e4167cfec37a08a5982faab0ed
Pavel Terekhov <pterekhov@gds000589-nb.carina.griddynamics.net>
2019-04-01 16:58:12 +0400

    task2, task4 - finished

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-init
+task1 - in progress
+task2 - finished
+task3 - start
+task4 - finished
+task5 - waiting
--- practices/2-linreg/task2.py
+++ practices/2-linreg/task2.py
@@ -1,25 +1,31 @@
-import pandas as pd
 from glob import glob
 import os
 import random
-
-from keras.applications.resnet50 import ResNet50
+import keras.applications.resnet50
 from keras.preprocessing import image
-from keras.applications.resnet50 import preprocess_input, decode_predictions
+from keras.applications.resnet50 import preprocess_input
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-random.seed(34)
+random.seed(42)
+
+begin = "\n------BEGIN------\n"
+end = "\n-------END-------\n"
+
+print("Path to data: ")
+path = input()
+
+#path = "/Users/pterekhov/Projects/PycharmProjects/ML_SSU/data"
+#print("Path to data:", path)
 
 
 def read_dataset(path):
     X = []
     y = []
 
-    image_paths_list = glob(os.path.join('train', '*.jpg'))
+    image_paths_list = glob(os.path.join(path,'train', '*.jpg'))
     image_paths_sample = random.sample(image_paths_list, 1000)
 
     for image_path in image_paths_sample:
@@ -37,29 +43,29 @@ def read_dataset(path):
 
     return np.array(X), y
 
-
-X, y = read_dataset('data')
-model = ResNet50(weights='imagenet')
+X, y = read_dataset(path)
+model = keras.applications.resnet50.ResNet50(weights='imagenet')
 preds = model.predict(X)
 
-preds_train, preds_test, y_train, y_test = train_test_split(
-preds, y, test_size=0.33, random_state=42)
+print("preds: ", begin, preds, end)
 
-clf = LogisticRegression(random_state=42,solver = 'lbfgs', multi_class = 'multinomial').fit(preds_train, y_train)
-
+preds_train, preds_test, y_train, y_test = train_test_split(preds, y, test_size=0.33, random_state=42)
+clf = LogisticRegression(random_state=42, solver='lbfgs', multi_class='multinomial').fit(preds_train, y_train)
 predictions = clf.predict(preds_test)
-accuracy_score(y_test, predictions)
 
-confusion_matrix(y_test, predictions)
+print("accuracy score: ", begin, accuracy_score(y_test, predictions), end)
+print("confusion matrix: ", begin, confusion_matrix(y_test, predictions), end)
 
-X_train, X_test, y_train, y_test = train_test_split(
-X, y, test_size=0.33, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 X_train = [x.flatten() for x in X_train]
 X_test = [x.flatten() for x in X_test]
+clf = LogisticRegression(random_state=42,solver='lbfgs', multi_class='multinomial').fit(X_train, y_train)
+predictions=clf.predict(X_test)
 
-clf = LogisticRegression(random_state=42,solver = 'lbfgs', multi_class = 'multinomial').fit(X_train, y_train)
-print(predictions = clf.predict(X_test),  end='nnn')
-print(accuracy_score(y_test, predictions), end='nnn')
-print(confusion_matrix(y_test, predictions),  end='nnn')
+print("predictions: ", begin, predictions, end)
+print("accuracy score: ", begin, accuracy_score(y_test, predictions), end)
+print("confusion matrix: ", begin, confusion_matrix(y_test, predictions), end)
 
+#/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages/keras
+#/Users/pterekhov/Projects/PycharmProjects/ML_SSU/data
 
